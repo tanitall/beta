a8e5e8bfa7f6a1c8a2ff5d6963bad3668d0b9bf2
AaronBesson <aaronbesson@gmail.com>
2017-12-17 12:24:30 -0400

    pro-chart

--- app/components/Trade.js
+++ app/components/Trade.js
@@ -1,45 +1,168 @@
 import React, { Component } from "react";
+import { Link } from "react-router";
 import { connect } from "react-redux";
-import MdSync from "react-icons/lib/md/sync";
-import { initiateGetBalance, intervals } from "../components/NetworkSwitch";
-import { resetPrice } from "../modules/wallet";
-import { sendEvent, clearTransactionEvent } from "../modules/transactions";
+import { setBlockExplorer } from "../modules/metadata";
+import { setKeys } from "../modules/account";
+import Delete from "react-icons/lib/md/delete";
+import _ from "lodash";
+import fs from "fs";
+import storage from "electron-json-storage";
+import Logo from "./Brand/LogoBlank";
+import NeoLogo from "./Brand/Neo";
+import Claim from "./Claim";
+import { NetworkSwitch } from "../components/NetworkSwitch";
+import { syncTransactionHistory } from "../components/NetworkSwitch";
 import { clipboard } from "electron";
+import Copy from "react-icons/lib/md/content-copy";
+import ReactTooltip from "react-tooltip";
+import TopBar from "./TopBar";
 
-class Trade extends Component {
-  componentDidMount = () => {
-    initiateGetBalance(this.props.dispatch, this.props.net, this.props.address);
-    QRCode.toCanvas(this.canvas, this.props.address, { version: 5 }, err => {
-      if (err) console.log(err);
-    });
-  };
+let explorer_select;
 
-  render = () => {
-    if (this.props.address != null) {
-      return (
-      <div>
-      <div id="exchange-info">
-        <webview
-          id="foo"
-          src={`https://www.morpheuswallet.com/charts/`}
-        />
-      </div>
-      </div>
-      );
-    } else {
-      return null;
+const { dialog } = require("electron").remote;
+const saveKeyRecovery = keys => {
+  const content = JSON.stringify(keys);
+  dialog.showSaveDialog(
+    {
+      filters: [
+        {
+          name: "JSON",
+          extensions: ["json"]
+        }
+      ]
+    },
+    fileName => {
+      if (fileName === undefined) {
+        console.log("File failed to save...");
+        return;
+      }
+      // fileName is a string that contains the path and filename created in the save file dialog.
+      fs.writeFile(fileName, content, err => {
+        if (err) {
+          alert("An error ocurred creating the file " + err.message);
+        }
+        alert("The file has been succesfully saved");
+      });
     }
+  );
+};
+
+const loadKeyRecovery = dispatch => {
+  dialog.showOpenDialog(fileNames => {
+    // fileNames is an array that contains all the selected
+    if (fileNames === undefined) {
+      console.log("No file selected");
+      return;
+    }
+    const filepath = fileNames[0];
+    fs.readFile(filepath, "utf-8", (err, data) => {
+      if (err) {
+        alert("An error ocurred reading the file :" + err.message);
+        return;
+      }
+      const keys = JSON.parse(data);
+      storage.get("keys", (error, data) => {
+        _.each(keys, (value, key) => {
+          data[key] = value;
+        });
+        dispatch(setKeys(data));
+        storage.set("keys", data);
+      });
+    });
+  });
+};
+
+const saveSettings = settings => {
+  storage.set("settings", settings);
+};
+
+const loadSettings = dispatch => {
+  storage.get("settings", (error, settings) => {
+    if (
+      settings.blockExplorer !== null &&
+      settings.blockExplorer !== undefined
+    ) {
+      dispatch(setBlockExplorer(settings.blockExplorer));
+    }
+  });
+};
+
+const updateSettings = dispatch => {
+  saveSettings({ blockExplorer: explorer_select.value });
+  dispatch(setBlockExplorer(explorer_select.value));
+};
+
+const deleteWallet = (dispatch, key) => {
+  storage.get("keys", (error, data) => {
+    delete data[key];
+    storage.set("keys", data);
+    dispatch(setKeys(data));
+  });
+};
+
+const getExplorerLink = (net, explorer, txid) => {
+  let base;
+  if (explorer === "Neotracker") {
+    if (net === "MainNet") {
+      base = "https://neotracker.io/tx/";
+    } else {
+      base = "https://testnet.neotracker.io/tx/";
+    }
+  } else {
+    if (net === "MainNet") {
+      base = "http://antcha.in/tx/hash/";
+    } else {
+      base = "http://testnet.antcha.in/tx/hash/";
+    }
+  }
+  return base + txid;
+};
+
+// helper to open an external web link
+const openExplorer = srcLink => {
+  shell.openExternal(srcLink);
+};
+
+class Settings extends Component {
+  componentDidMount = () => {
+    storage.get("keys", (error, data) => {
+      this.props.dispatch(setKeys(data));
+    });
+    syncTransactionHistory(
+      this.props.dispatch,
+      this.props.net,
+      this.props.address
+    );
+    loadSettings(this.props.dispatch);
   };
+
+  render = () => (
+    <div id="send">
+      <TopBar />
+      <div className="settings-panel top-50 fadeInDown">
+      <webview
+        id="tradingview"
+        src={`https://www.morpheuswallet.com/charts/`}
+      />
+      </div>
+
+      <div className="clearboth" />
+    </div>
+  );
 }
 
 const mapStateToProps = state => ({
-  neo: state.wallet.Neo,
-  gas: state.wallet.Gas,
+  explorer: state.metadata.blockExplorer,
+  wallets: state.account.accountKeys,
+  blockHeight: state.metadata.blockHeight,
   address: state.account.address,
   net: state.metadata.network,
-  price: state.wallet.price
+  neo: state.wallet.Neo,
+  gas: state.wallet.Gas,
+  price: state.wallet.price,
+  transactions: state.wallet.transactions
 });
 
-Trade = connect(mapStateToProps)(Trade);
+Settings = connect(mapStateToProps)(Settings);
 
-export default Trade;
+export default Settings;
